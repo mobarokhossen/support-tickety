21d9fd7b55f3e15e1b5401981f2da507e98ed45d
mobarok <mobarokaiub@gmail.com>
2024-07-06 16:41:32 +0600

    service provider updates

--- README.md
+++ README.md
@@ -1,8 +1,61 @@
-# SupportTickety
+[![Latest Version](https://img.shields.io/github/release/mobaroklab/support-tickety.svg)](https://github.com/mobaroklab/support-tickety/releases)
+[![Total Downloads](https://img.shields.io/packagist/dt/mobaroklab/support-tickety.svg)](https://packagist.org/packages/mobaroklab/support-tickety)
+[![Software License](https://img.shields.io/badge/license-MIT-brightgreen.svg)](LICENSE)
 
-A Laravel package for managing support tickets and ticket categories.
+# Support Tickety
+
+A Laravel package for managing support tickets.
+
+## Features
+* Create, view, update, and delete support tickets
+* Assign tickets to users
+* Track the status of tickets (open, closed, in-progress)
+* Notifications for ticket updates
+* Flexible and extendable
+
+## Laravel Versions
+
+Laravel | Support Tickety
+--- | ---
+5.6 - 10.x | 1.*
 
 ## Installation
 
+### Step 1: Composer
+
+You can install the package via composer:
+
 ```bash
 composer require mobaroklab/support-tickety
+```
+```bash
+"require": {
+    "mobaroklab/support-tickety": "~1.0"
+}
+```
+#### OR  place manually in composer.json:
+
+```bash
+"require": {
+    "mobaroklab/support-tickety": "~1.0"
+}
+```
+```bash
+composer update
+```
+### Step 2: Service Provider
+Add the service provider to config/app.php under providers:
+
+```bash
+php artisan vendor:publish --provider="MobarokLab\SupportTickety\SupportTicketServiceProvider"
+
+```
+
+
+#### OR  place manually
+
+```bash
+'providers' => [
+    MobarokLab\SupportTickety\SupportTicketServiceProvider::class,
+],
+```
--- src/SupportTicketServiceProvider.php
+++ src/SupportTicketServiceProvider.php
@@ -10,23 +10,23 @@ class SupportTicketServiceProvider extends ServiceProvider
     {
         // Publish configuration
         $this->publishes([
-            __DIR__ . '/../config/support-ticket.php' => config_path('support-ticket.php'),
+            __DIR__ . '/config/support-ticket.php' => config_path('support-ticket.php'),
         ], 'config');
 
         // Publish migrations
         if (!class_exists('CreateSupportTicketsTable')) {
             $this->publishes([
-                __DIR__ . '/../database/migrations/create_support_tickets_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_support_tickets_table.php'),
-                __DIR__ . '/../database/migrations/create_support_categories_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_support_categories_table.php'),
+                __DIR__ . '/database/migrations/create_support_tickets_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_support_tickets_table.php'),
+                __DIR__ . '/database/migrations/create_support_categories_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_support_categories_table.php'),
             ], 'migrations');
         }
 
         // Load the views
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'support-tickety');
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'support-tickety');
 
         // Publish the views
         $this->publishes([
-            __DIR__ . '/../resources/views' => resource_path('views/vendor/support-tickety'),
+            __DIR__ . '/resources/views' => resource_path('views/vendor/support-tickety'),
         ]);
     }
 
@@ -34,7 +34,7 @@ class SupportTicketServiceProvider extends ServiceProvider
     {
         // Merge configuration
         $this->mergeConfigFrom(
-            __DIR__ . '/../config/support-ticket.php', 'support_tickety'
+            __DIR__ . '/config/support-ticket.php', 'support_tickety'
         );
     }
 }
